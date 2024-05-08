7f273bfe8e8a4c1edae78a899f37c0504d41302e
Sonu Vyavahare <sonuvyavahare2021@gmail.com>
2024-05-08 16:07:27 +0530

    Assignment1

---
+++ src/sample/Assign1.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class Assign1 {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
--- src/sample/SampleTest.java
+++ src/sample/SampleTest.java
@@ -1,6 +1,7 @@
 package sample;
 
 public class SampleTest {
+
 	
 	public static void main(String[] args) {
 		System.out.println("Test Sample");
